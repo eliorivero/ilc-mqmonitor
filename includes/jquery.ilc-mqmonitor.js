5fbdb7d933f3dbe772ba3c382da81b90c1d811bf
eliorivero <User@.(none)>
2012-06-28 10:45:58 -0300

    clean commit

---
+++ includes/jquery.ilc-mqmonitor.js
@@ -0,0 +1,27 @@
+/**
+ * Media Queries Monitor script for WordPress Admin Bar
+ * Created by ilovecolors.com.ar
+ * Follow me on Twitter twitter.com/eliorivero 
+ */
+
+jQuery(document).ready(function($) {
+	
+	$('#wp-admin-bar-root-default').prepend('<li><a class="mqmonitor" href="#">ILC Media Queries</a></li>');
+	
+	ilcmqs = ilcmqm.mq.split(',');
+	
+	if( $(window).width() > ilcmqm.fs )
+		$('.mqmonitor').text( ilcmqm.fs );
+	
+	for( i = 0; i < ilcmqs.length; i++ )
+		if ( $(window).width() < ilcmqs[i] )
+			$('.mqmonitor').text( ilcmqs[i] );
+	
+	$(window).resize(function(){
+		
+		for( i = 0; i < ilcmqs.length; i++ )
+			if ( $(window).width() < ilcmqs[i] )
+				$('.mqmonitor').text( ilcmqs[i] );
+	});
+	
+});
